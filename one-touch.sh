53c2bb1c0e797af050fead0016315ee9056bb054
tracy1949 <63947743+tracy1949@users.noreply.github.com>
2023-02-03 12:29:13 +0800

    Update one-touch.sh

--- one-touch.sh
+++ one-touch.sh
@@ -7,10 +7,10 @@ cd ${WORKSPACE}
 
 # 下载, arm 机器替换 x86_64 为 aarch64
 OS_ARCH="aarch64"
-latest_version=$(curl -m 10 -sL "https://api.github.com/repos/zdz/ServerStatus-Rust/releases/latest" | grep "tag_name" | head -n 1 | awk -F ":" '{print $2}' | sed 's/\"//g;s/,//g;s/ //g')
+latest_version=$(curl -m 10 -sL "https://api.github.com/repos/tracy1949/ServerStatus-Rust/releases/latest" | grep "tag_name" | head -n 1 | awk -F ":" '{print $2}' | sed 's/\"//g;s/,//g;s/ //g')
 
-wget --no-check-certificate -qO "server-${OS_ARCH}-unknown-linux-musl.zip"  "https://github.com/zdz/ServerStatus-Rust/releases/download/${latest_version}/server-${OS_ARCH}-unknown-linux-musl.zip"
-wget --no-check-certificate -qO "client-${OS_ARCH}-unknown-linux-musl.zip"  "https://github.com/zdz/ServerStatus-Rust/releases/download/${latest_version}/client-${OS_ARCH}-unknown-linux-musl.zip"
+wget --no-check-certificate -qO "server-${OS_ARCH}-unknown-linux-musl.zip"  "https://github.com/tracy1949/ServerStatus-Rust/releases/download/${latest_version}/server-${OS_ARCH}-unknown-linux-musl.zip"
+wget --no-check-certificate -qO "client-${OS_ARCH}-unknown-linux-musl.zip"  "https://github.com/tracy1949/ServerStatus-Rust/releases/download/${latest_version}/client-${OS_ARCH}-unknown-linux-musl.zip"
 
 unzip -o "server-${OS_ARCH}-unknown-linux-musl.zip"
 unzip -o "client-${OS_ARCH}-unknown-linux-musl.zip"
